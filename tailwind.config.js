a15fb25e8e50f0c4eed63a4b57d846e16afad8d8
Viacheslav Chykov <viacheslav.chykov@gmail.com>
2023-08-06 05:09:58 +0300

    Creating versions of layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '2': 'rgba(99, 99, 99, 0.2) 0px 0px 10px 0px;',
+        '3': 'rgba(99, 99, 99, 0.2) 0px 0px 20px 0px;',
+      }
+    }
   },
   plugins: [],
 }
