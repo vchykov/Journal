85d45e8abefa7ea802ee4fd6f5e0ac5718dd91e5
Viacheslav Chykov <viacheslav.chykov@gmail.com>
2023-08-25 04:37:27 +0300

    Updates db.json

--- src/script.js
+++ src/script.js
@@ -4,13 +4,13 @@ var md = window.markdownit({
     highlight: function (str, lang) {
       if (lang && hljs.getLanguage(lang)) {
         try {
-          return '<pre class="hljs">' +
+          return '<pre class="hljs"><code>' +
                  hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
-                 '</pre>';
+                 '</code></pre>';
         } catch (__) {}
       }
   
-      return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+      return '<pre class="hljs">' + md.utils.escapeHtml(str) + '</pre>';
     }
   }).use(window.markdownitSup).use(window.markdownitSub);
 
