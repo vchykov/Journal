ad34d2bc3376df8a6dcbb79e69cfc5d21b6102b5
Viacheslav Chykov <viacheslav.chykov@gmail.com>
2023-09-03 18:48:53 +0300

    Fixes layout. Flex works well

--- src/script.js
+++ src/script.js
@@ -34,54 +34,56 @@ function posts(parentSelector) {
             const element = document.createElement('div');
             element.innerHTML = `
 
-        <div class="border-blue-600 pr-7 sm:pr-5 grid grid-cols-[72px,_minmax(200px,_auto)] 
-            sm:grid-cols-[42px,_minmax(200px,_auto)] grid-rows-[32px,_minmax(100px,_auto)])">
-            <div class="flex flex-row h-8 sm:h-6">
-                ${(this.type === 'successful') ? `
-                <img src="./img/successful.svg" alt="Successful" class="mx-auto w-8 h-8 sm:h-6 sm:w-6" />
-                ` : ''}
-                ${(this.type === 'problem') ? `  
-                <img src="./img/problem.svg" alt="Problem" class="mx-auto w-8 h-8 sm:h-6 sm:w-6" />
-                ` : ''}
-                ${(this.type === 'fail') ? `
-                <img src="./img/fail.svg" alt="Fail" class="mx-auto w-8 h-8 sm:h-6 sm:w-6" />
-                ` : ''}  
-            </div>
-            <div class="h-8 sm:h-6 flex lex-col">
-                <span class="self-center max-w-fit px-2 rounded-md bg-[#007bff] text-white text-xs font-bold">
-                    ${this.date}
-                </span>
-            </div>
-            <div class="flex-grow flex flex-row ${(this.last) ? 'invisible' : ''}">
-                <div class="w-px border-gray-300 border-r mx-auto"></div>
-            </div>
-            <div class="pb-4 pt-1 ">
-                <div class="p-3 sm:p-1 mx-auto rounded-md shadow-2 hover:shadow-3">
-                    <div class="px-1.5 text-lg font-bold">${this.title}</div>
-                    <div class="p-1.5 ">
-                        ${this.text}
+                <div class="pr-7 sm:pr-5 flex flex-row">
+                    <div class="px-4 sm:px-1.5 w-16 sm:w-9 mx-1 sm:mx-0.5 flex-none border-red-600 flex flex-col">
+                        <div class="flex-none h-8 sm:h-6 ">
+                            <div class="h-8 w-8 sm:h-6 sm:w-6">
+                            ${(this.type === 'successful') ? `
+                            <img src="./img/successful.svg" alt="Successful" class="mx-auto w-8 h-8 sm:h-6 sm:w-6" />
+                            ` : ''}
+                            ${(this.type === 'problem') ? `  
+                            <img src="./img/problem.svg" alt="Problem" class="mx-auto w-8 h-8 sm:h-6 sm:w-6" />
+                            ` : ''}
+                            ${(this.type === 'fail') ? `
+                            <img src="./img/fail.svg" alt="Fail" class="mx-auto w-8 h-8 sm:h-6 sm:w-6" />
+                            ` : ''}  
+                            </div>
+                        </div>
+                        <div class="flex-grow flex flex-row ${(this.last) ? 'invisible' : ''}">
+                            <div class="w-px border-gray-300 border-r mx-auto"></div>
+                        </div>
                     </div>
-                    <div class="p-1.5" id="${this.date}" style="display:none" >
-                        ${this.textUnderSpoiler}
+                    <div class="flex-grow min-w-0">
+                        <div class="h-8 sm:h-6 flex items-center">
+                            <span class="max-w-fit px-2 rounded-md bg-tl-blue text-white text-xs font-bold">
+                                ${this.date}
+                            </span>
+                        </div>
+                        <div class="pb-4 pt-1">
+                            <div class="p-3 sm:p-1 mx-auto bg-white rounded-md shadow-2 hover:shadow-3">
+                                <div class="px-1.5 text-lg font-bold">${this.title}</div>
+                                <div class="p-1.5">
+                                    ${this.text}
+                                </div>
+                                <div class="p-1.5" id="${this.date}" style="display:none" >
+                                    ${this.textUnderSpoiler}
+                                </div>
+                                <button class="p-1.5 text-tl-blue" title="Click to show/hide content" 
+                                    type="button" ${(this.textUnderSpoiler === undefined || this.textUnderSpoiler === '') ? `style="display:none"` : ''} 
+                                    onclick="
+                                        if(document.getElementById('${this.date}').style.display=='none') {
+                                        document.getElementById('${this.date}').style.display='';
+                                        document.getElementById('${this.date}').nextElementSibling.innerHTML='Hide';
+                                        } else {
+                                        document.getElementById('${this.date}').style.display='none';
+                                        document.getElementById('${this.date}').nextElementSibling.innerHTML='Show more';
+                                    }
+                                ">Show more
+                                </button>
+                            </div>
+                        </div>
                     </div>
-                    <button class="p-1.5 text-[#007bff]" title="Click to show/hide content" 
-                        type="button" ${(this.textUnderSpoiler === undefined || this.textUnderSpoiler === '') ? `style="display:none"` : ''} 
-                            onclick="
-                                if(document.getElementById('${this.date}').style.display=='none') {
-                                    document.getElementById('${this.date}').style.display='';
-                                    document.getElementById('${this.date}').nextElementSibling.innerHTML='Hide';
-                                } else {
-                                document.getElementById('${this.date}').style.display='none';
-                                document.getElementById('${this.date}').nextElementSibling.innerHTML='Show more';
-                            }
-                        ">Show more
-                    </button>
                 </div>
-            </div>
-        </div> 
-
-
-                
             `;
             this.parent.append(element);
         }
